fd06eca069d2f405b5441038d32a11d3adda68a8
alexandr <alexpod24@gmail.com>
2023-03-08 12:31:00 +0300

    add Exemple 005 conditiomIfElse

---
+++ Exemple005_conditiomIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "кисочка")
+{
+    Console.WriteLine("ура это же Кисочка" );
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
