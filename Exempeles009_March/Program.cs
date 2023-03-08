dbae7f8f26968abf9848b3c0e6206d1814bb36d5
alexandr <alexpod24@gmail.com>
2023-03-08 22:08:47 +0300

    add 010

---
+++ Exempeles009_March/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int[] arrey = { 13, 24, 36, 14, 54, 16, 37, 38, 14 };
+
+int n = arrey.Length;
+int find = 14;
+int index = 0;
+while (index < n)
+{
+    if (arrey[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+        index++;
+}
